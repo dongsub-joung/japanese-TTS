1c15dbe45185f8d9be27c3d4c92edab0df237084
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-11 01:17:39 +0900

    update

--- zundamon/src/Windows.rs
+++ zundamon/src/Windows.rs
@@ -6,16 +6,8 @@ use crate::Windows::ClipBoard;
 
 pub mod ClipBoard{
     pub fn get_it() -> String{
-        const SAMPLE: &'static str= "asdfasdfas";
-        let _clip = Clipboard::new_attempts(10).expect("Open clipboard");
-        formats::Unicode.write_clipboard(&SAMPLE).expect("Write sample");
+        let clip_text= Clipboard::get_it();
 
-        let mut output = String::new();
-
-        let clip_text= formats::Unicode.read_clipboard(&mut output).expect("Read sample");
-   
-        // output.clear();
-        
         clip_text
     }
 }
