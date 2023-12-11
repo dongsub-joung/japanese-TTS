41d4102e3d4c268fd08ccdd98d3afe52298d79b0
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-12 02:08:16 +0900

    refactoring windows.rs

--- zundamon/src/windows.rs
+++ zundamon/src/windows.rs
@@ -1,7 +1,4 @@
-// use clipboard_win::Clipboard;
-
 pub mod clip_board{
-    
     pub fn get_it() -> String{
         let clip_text= clipboard_win::get_clipboard_string()
             .expect("clipboard io err");
