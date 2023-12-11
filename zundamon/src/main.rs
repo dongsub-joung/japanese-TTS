f111d92aba14e55e2f61c779578d1ec56c322b32
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-12 01:47:44 +0900

    done

--- zundamon/src/main.rs
+++ zundamon/src/main.rs
@@ -7,6 +7,8 @@ use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
+use soloud::*;
+
 fn init() ->  Result<(), Box<dyn std::error::Error>> {
     let text= windows::clipBoard::get_it();
     
@@ -22,7 +24,21 @@ fn init() ->  Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+
+fn play_audio(pwd: &str){
+    let sl = Soloud::default().unwrap();
+    let mut wav = audio::Wav::default();
+    // wav.load_mem(include_bytes!("C:/Users/kiririn/git/japanese-TTS/zundamon/audio.wav")).unwrap();
+    wav.load(pwd);
+    sl.play(&wav);
+    while sl.voice_count() > 0 {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+}
+
 fn main() {
+    const path: &str= "C:/Users/kiririn/git/japanese-TTS/zundamon/audio.wav";
+    
     // A flag to indicate when to stop the loop
     let running = Arc::new(AtomicBool::new(true));
     let running_clone = Arc::clone(&running);
@@ -40,9 +56,10 @@ fn main() {
         println!("Running...");
 
         let string= windows::clipBoard::get_it();
-        thread::sleep(Duration::from_secs(10));
+        thread::sleep(Duration::from_secs(5));
 
         let _= init();
+        play_audio(path);
 
         // Simulating some work
         thread::sleep(Duration::from_secs(10));
