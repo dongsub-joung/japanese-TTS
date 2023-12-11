4a6ae3c5f8d2dfe9fd8308ec3f98a5abd098a7ad
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-11 11:12:25 +0900

    clip

--- zundamon/src/Windows.rs
+++ zundamon/src/Windows.rs
@@ -1,13 +1,9 @@
 use clipboard_win::Clipboard;
-use clipboard_win::formats;
-
-use crate::Windows::formats;
-use crate::Windows::ClipBoard;
 
 pub mod ClipBoard{
     pub fn get_it() -> String{
-        let clip_text= Clipboard::get_it();
-
+        let clip_text= clipboard_win::get_clipboard_string().unwrap();
+        
         clip_text
     }
 }
