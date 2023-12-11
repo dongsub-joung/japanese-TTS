3d873da363e151989a051b854a72ea7175f7b5b4
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-12 02:46:31 +0900

    refactoring

--- zundamon/src/voicevox_request.rs
+++ zundamon/src/voicevox_request.rs
@@ -1,30 +1,40 @@
 use std::fs::File;
 use std::io::Read;
-// use reqwest::{Client, Error};
 use std::collections::HashMap;
 use serde_urlencoded;
 use reqwest::header::{CONTENT_TYPE, CONNECTION};
 
 pub mod save {
+    use super::*;
     use std::fmt::format;
 
-    use super::*;
-    const BASE_URL: &str= "http://localhost:50021";
+    const BASE: &str= "http://localhost:50021";
+    const SPEAKER: &str= "1";
+
+    fn open_then_read()-> Result<(), Box<dyn std::error::Error>> {
+        let mut text = String::new();
+        
+        let mut file = std::fs::File::open("text.txt")?;
+        file.read_to_string(&mut text)
+            .expect("{can't read a text file}");
+
+        Ok(())
+    }
+
+    fn set_hashmap_value() -> HashMap<&str, &str>{
+        vec![("text", text.as_str()), ("speaker", "1")].into_iter().collect()
+    }
 
     pub fn json_init() -> Result<(), Box<dyn std::error::Error>> {
         let client = reqwest::blocking::Client::new();
         
-        // Read the contents of text.txt file
-        let mut file = std::fs::File::open("text.txt")?;
-        let mut text = String::new();
-        file.read_to_string(&mut text)?;
+        open_then_read();
     
-        let url = "http://localhost:50021/audio_query";
-
-        let m: HashMap<&str, &str>= vec![
-            ("text", text.as_str()), ("speaker", "1")].into_iter().collect();
-
+        // let url = "http://localhost:50021/audio_query";
+        let url= format!("{}/audio_query", BASE);
+        let m: HashMap<&str, &str>= set_hashmap_value();
         let params= serde_urlencoded::to_string(m)?;
+        
         let mut response = client
             .post(format!("{}?{}", url, params)).send()?;
     
@@ -37,7 +47,9 @@ pub mod save {
     pub fn audio_init() -> Result<(), Box<dyn std::error::Error>> {
         let client = reqwest::blocking::Client::new();
     
-        let url = "http://localhost:50021/synthesis?speaker=1";
+        // let url = "http://localhost:50021/synthesis?speaker=1";
+        let url= format!("{}/synthesis?speaker={}", BASE, SPEAKER);
+
         let mut file = File::open("query.json")?;
         let mut json_content = String::new();
         file.read_to_string(&mut json_content)?;
