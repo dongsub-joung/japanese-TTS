9e04f358f2c179127116a2d04901f7b80506952e
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-11 23:00:30 +0900

    del testing

--- testing/src/main.rs
+++
@@ -1,8 +0,0 @@
-use voicevox::{*, client::VVClient};
-
-fn main() {
-    let vvc= VVClient::new();
-    let qs= vvc.query(txt, id);
-    let dat = vvc.synth(qs, id).unwrap();
-    vvc.speak(dat, 3).unwrap();
-}
